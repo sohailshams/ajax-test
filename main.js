874eb6ea15e2bba2dbe113f222b54a89ef51c749
sohailshams <sohailshamas@yahoo.com>
2020-04-22 08:36:46 +0000

    Initial Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText;
+    }
+};
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+
+xhr.send();
